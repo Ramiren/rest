5e7e01662d716b739d17f1f95e6dd0b766a411ae
Ramiren <tobiashendry@gmail.com>
2022-07-14 20:32:58 +0200

    Add files via upload

---
+++ w.rb
@@ -0,0 +1,67 @@
+def divident_wareneingang
+  puts "Wieviele Einheiten insgesamt"
+  puts
+  print "Einheiten insgesamt: "
+  begin
+  divident = Integer(gets.chomp)
+  rescue
+    print "Nur Zahlen, bitte. Einheiten insgesamt: "
+    retry
+  end
+  divident
+end
+
+def divisor_wareneingang
+  print "Einheiten pro Palette: "
+  begin
+  divisor = Integer(gets.chomp)
+  if divisor == 0
+    puts "Sehr schlechte Idee"
+    puts
+    main_wareneingang
+  end
+  rescue
+    print "Einheiten pro Palette:: "
+    retry
+  end
+  divisor
+end
+
+def berechnung (divident, divisor) 
+  return divident / divisor, divident % divisor
+end
+
+def ausgabe_wareneingang (quotient_rest) 
+  quotient = quotient_rest.at(0)
+  rest = quotient_rest.at(1)
+  if rest == 0
+    puts "#{quotient} UMK"
+    elsif quotient == 0
+    puts "#{rest} Einheiten"
+    else
+    puts "#{quotient} Paletten, #{rest} Einheiten"
+  end
+  abfrage
+end
+
+def abfrage
+  puts
+  print "Nochmals? (j/n) "
+  eingabe = gets.chomp
+  eingabe.downcase!
+  if eingabe == "n"
+    exit
+  else 
+    puts
+    main_wareneingang
+  end
+end
+def main_wareneingang
+puts "Berechnet die Anzahl und den Rest beim Wareneingang"
+  puts
+  ausgabe_wareneingang(berechnung(divident_wareneingang,divisor_wareneingang))
+  abfrage
+end
+
+main_wareneingang
+
